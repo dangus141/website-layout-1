2b2e4520d9d3b71329ae2cf1d81fb7158ceb551e
Dan <dlunaria@asu.edu>
2026-05-13 23:01:08 -0700

    test

--- .eleventy.js
+++ .eleventy.js
@@ -1,26 +1,49 @@
 module.exports = function (eleventyConfig) {
-  // This makes the eleventy command quieter (with less detail)
+
+  // --- 1. FILTERS ---
+
+  // Your stripSlash filter
+  eleventyConfig.addFilter("stripSlash", (url) => {
+    if (typeof url !== "string") return url;
+    return url.startsWith("/") ? url.substring(1) : url;
+  });
+
+  // Your readableDate filter
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    if (!dateObj) return "Date not available";
+    return new Date(dateObj).toLocaleDateString();
+  });
+
+  // The Excerpt filter 
+  eleventyConfig.addFilter("excerpt", (post) => {
+    if (!post) return "";
+    const content = post.replace(/(<([^>]+)>)/gi, ""); // Remove HTML tags
+    return content.substr(0, content.lastIndexOf(" ", 200)) + "..."; 
+  });
+
+
+  // --- 2. SETTINGS & PASSTHROUGH ---
+
   eleventyConfig.setQuietMode(true);
-
-  // This will stop the default behaviour of foo.html being turned into foo/index.html
   eleventyConfig.addGlobalData("permalink", "{{ page.filePathStem }}.html");
-
-  // This will make Eleventy not use your .gitignore file to ignore files
   eleventyConfig.setUseGitIgnore(false);
+  
+  // This ensures your images are copied to the output folder
+  eleventyConfig.addPassthroughCopy({ "src/public": "/" });
+  
+  eleventyConfig.setTemplateFormats(["html", "njk", "txt", "js", "css", "xml", "json", "md"]);
 
-  // This will copy this folder to the output without modifying it at all
-  eleventyConfig.addPassthroughCopy("content/assets");
+  // --- 3. RETURN CONFIG ---
 
-  // This defines which files will be copied
-  eleventyConfig.setTemplateFormats(["html", "njk", "txt", "js", "css", "xml", "json"]);
-
-  // This defines the input and output directories
   return {
-    // This makes sure HTML files use Nunjucks
     htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk", // Added this so Nunjucks works inside Markdown files
     dir: {
       input: "content",
+      includes: "_includes",
       output: "public",
     },
   };
+
+  //
 };
